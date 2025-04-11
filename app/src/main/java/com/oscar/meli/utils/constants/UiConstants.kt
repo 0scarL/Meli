6abd2dfb6d70f7e5f4b492a11da5493e42d2cc6a
0scarL <o_lizarazob@hotmail.com>
2025-04-11 18:31:45 -0500

    crea layout y strings de xmls

---
+++ app/src/main/java/com/oscar/meli/utils/constants/UiConstants.kt
@@ -0,0 +1,9 @@
+package com.oscar.meli.utils.constants
+
+object UiConstants {
+
+    //region product
+    const val MJS_ERROR = "Ocurrió un error"
+    //endregion
+
+}
--- app/src/main/java/com/oscar/meli/utils/constants/several.kt
+++ app/src/main/java/com/oscar/meli/utils/constants/several.kt
@@ -1,5 +1,6 @@
 package com.oscar.meli.utils.constants
 
 object several {
-    const val TOKEN = "Bearer APP_USR-5483997440462626-041021-252cea0c0b1a2f291e262dfaaf92f122-18490503"
+    const val TOKEN = "Bearer APP_USR-5483997440462626-041119-037d4e46ecf2927793f46d5c80cf17d7-18490503"
 }
+
